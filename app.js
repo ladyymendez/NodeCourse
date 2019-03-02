44c7418cd13bea98094a172ebee6603044b99046
ladyymendez <ladyymendez@gmail.com>
2019-03-01 19:23:20 -0600

    Sending Responses Node

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const  http = require('http');
 
-const server = http.createServer((req,res)=>{console.log("Hi", req)});
+const server = http.createServer((req,res)=>{
+  console.log("Hi", req)
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`
+   <html>
+     <body>
+       Hola 
+     </body>
+   </html>
+  `)
+  res.end();
+
+});
 
 server.listen(3000);
