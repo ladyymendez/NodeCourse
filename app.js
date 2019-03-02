54ac97cb5ec49f624db159081e3112d59ac22e2a
ladyymendez <ladyymendez@gmail.com>
2019-03-01 20:20:17 -0600

    routing request

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const  http = require('http');
 
 const server = http.createServer((req,res)=>{
+
+  const url= req.url;
+  if(url==='/'){
   console.log("Hi", req)
   res.setHeader('Content-Type', 'text/html');
   res.write(`
    <html>
      <body>
-       Hola 
+      <form action="/message" method="POST" name="message">
+        <input type="text"> <button type="submit">Send</button></input>
+      </form>
+     </body>
+   </html>
+  `);
+  return res.end();
+  
+  }
+  console.log("Hi", req)
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`
+   <html>
+     <body>
+       Hello from Node js
      </body>
    </html>
   `)
