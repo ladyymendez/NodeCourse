b4648c210ce3670cd6c01e5c94226761ebdd6ac0
ladyymendez <ladyymendez@gmail.com>
2019-03-15 19:45:18 -0600

    Filtering PATHS

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,16 @@ const express= require('express');
 
 const router = express.Router();
 
+// /admin/add-product
 router.use('/add-product',(req,res,next) => {
     console.log("another middleware")
     
     //res.setHeader('Content-type/text');
-    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Poduct</button></button></form>");
+    res.send("<form action='/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Poduct</button></button></form>");
 });
-router.post('/product', (req,res,next)=>{
+
+// /admin/add-product
+router.post('/add-product', (req,res,next)=>{
     const {title}=req.body;
     console.log(title);
     res.redirect('/');
