a91d154ddc71044b4b0c58d25104c069365ad8ce
ladyymendez <ladyymendez@gmail.com>
2019-03-19 23:26:17 -0600

    Fetching Data from files via the model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products =[];
+const fs = require('fs');
+const path = require('path');
 
 
 module.exports = class Product {
@@ -7,10 +8,35 @@ module.exports = class Product {
   }
 
   save() {
-   products.push(this);
+   const p = path.join(
+     path.dirname(process.mainModule.filename),
+     'data', 
+     'products.json'
+     );
+     fs.readFile(p,(err,fileContent)=>{
+      let products = []; 
+      console.log(fileContent);
+      if(!err){
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p,JSON.stringify(products), (err)=>{
+        console.log(err);
+      });
+     });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data', 
+      'products.json'
+      );
+    fs.readFile(p,(err,fileContent)=>{
+      if(err){
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    })
   }
 };
