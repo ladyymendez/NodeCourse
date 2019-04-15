3d5daf5a5350bababe856cb420294dde8a7574e3
ladyymendez <ladyymendez@gmail.com>
2019-04-15 17:22:25 -0500

    Getting a single product with where condition

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,16 +18,27 @@ exports.getProducts = (req,res,next) => {
 exports.getProduct = (req,res,next) => {
     const prodId = req.params.productId;
     //res.redirect('/');
-    Product.findById(prodId)
-    .then(([product]) => {
+    /*Product.findAll({where: {id:prodId}})
+    .then(products => {
         res.render('shop/product-detail',
         {
-            product: product[0],
-            pageTitle:'Product Detail', 
-            path: '/product'
+            product: products[0],
+            pageTitle:products[0].title, 
+            path: '/products'
         })
     })
-    .catch(err => console.log(err));
+    .catch(console.log);*/
+
+    Product.findByPk(prodId)
+    .then(product => {
+        res.render('shop/product-detail',
+        {
+            product: product,
+            pageTitle:'Product Detail', 
+            path: '/products'
+        })
+    })
+    .catch(console.log); 
 
 };
 
