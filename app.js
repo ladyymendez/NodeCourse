d08f88eabcb19f5e0498128f77bc8ba90a442a81
ladyymendez <ladyymendez@gmail.com>
2019-04-23 15:18:58 -0500

    One-To-Many Many-To-Many relation

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 app.set('view engine','pug');
 app.set('views', 'views');
@@ -36,6 +38,11 @@ app.use(errorController.get404Page);
 
 Product.belongsTo(User,{constrains: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
 sequelize
 .sync()
 .then(result => {
