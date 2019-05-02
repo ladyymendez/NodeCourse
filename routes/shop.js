34a50dd295ca54ebedda9a205b78f827412696a6
ladyymendez <ladyymendez@gmail.com>
2019-05-02 15:13:25 -0500

    Deleting cart items

--- routes/shop.js
+++ routes/shop.js
@@ -16,7 +16,7 @@ router.get('/cart',shopController.getCart);
 
 router.post('/cart',shopController.postCart);
 
-// router.post('/cart-delete-item',shopController.postCartDeleteProdct);
+router.post('/cart-delete-item',shopController.postCartDeleteProdct);
 
 // router.post('/create-order', shopController.postOrder);
 
